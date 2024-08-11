66736bb49c5852c85984139a79463e902f20b6c8
Arvee <arveedurante220@gmail.com>
2024-08-11 17:27:25 +0800

    Finished the project

--- Toast Notification or Snack Bar for Website/js/app.js
+++ Toast Notification or Snack Bar for Website/js/app.js
@@ -1,9 +1,27 @@
 const toastBox = document.getElementById('toastBox');
 
-function showToast() {
+const successMsg = 
+  '<i class="fa-solid fa-circle-check"></i> Successfully submitted';
+const errorMsg =
+  '<i class="fa-solid fa-circle-xmark"></i> Please fix the error!';
+const invalidMsg =
+  '<i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again';
+
+function showToast(msg) {
   let toast = document.createElement('div');
   toast.classList.add('toast');
-  toast.innerHTML = 'success';
+  toast.innerHTML = msg;
 
   toastBox.appendChild(toast);
+
+  if (msg.includes('error')) {
+    toast.classList.add('error');
+  }
+  if (msg.includes("Invalid")) {
+    toast.classList.add("invalid");
+  }
+
+  setTimeout(() => {
+    toast.remove();
+  }, 5000);
 }
