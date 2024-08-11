cd2ca347344a28afc4c476e0bdc70fc5e1ae4bb5
Arvee <arveedurante220@gmail.com>
2024-08-11 15:22:46 +0800

    add API link

--- QR code generator Website/js/app.js
+++ QR code generator Website/js/app.js
@@ -4,16 +4,17 @@ let qrText = document.getElementById("qrText");
 
 function generateQR() {
   if (!qrText.value) {
-    qrText.classList.add('error');
+    qrText.classList.add("error");
     setTimeout(() => {
-      qrText.classList.remove('error');
-    }, 1000); 
-    return
+      qrText.classList.remove("error");
+    }, 1000);
+    return;
   }
   
-  qrImage.src =`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`;
+  /* this API is from: https://goqr.me/api/ */
+  qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`;
 
-  imgBox.classList.add('show-img');
+  imgBox.classList.add("show-img");
 }
 
 document.getElementById('generateBtn').addEventListener('click', () => generateQR());
