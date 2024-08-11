9e1482255bb59d113d92d8ac401c4cf7ea77ecee
Arvee <arveedurante220@gmail.com>
2024-08-11 16:28:13 +0800

    added JS and create new Element div with a class toast

---
+++ Toast Notification or Snack Bar for Website/js/app.js
@@ -0,0 +1,9 @@
+const toastBox = document.getElementById('toastBox');
+
+function showToast() {
+  let toast = document.createElement('div');
+  toast.classList.add('toast');
+  toast.innerHTML = 'success';
+
+  toastBox.appendChild(toast);
+}
